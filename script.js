4f81a22d8f1bd900b3bd9aa193e1a19587959e60
Carmine Rodolfo Tagliafierro <carminerodolfotagliafierro@MacBook-Air-di-Carmine.local>
2024-02-17 13:50:31 +0100

    modifiche home

--- script.js
+++ script.js
@@ -19,3 +19,15 @@ window.addEventListener('scroll', () => {
     });
 });
 
+const services = document.querySelectorAll(".service");
+
+services.forEach(service => {
+    service.addEventListener("click", () => {
+        service.classList.toggle("active");
+    })
+})
+
+
+
+
+
